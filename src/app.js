a08db77bafc69ede5a7535a499b9e27335601882
itsmercify <gopuajith@gmail.com>
2021-08-27 09:11:55 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
 })
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000)
 
 console.clear();
 console.log("Running!");
